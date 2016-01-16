8e0190e540749bbe7b4a72aed169692f51ffdfb4
longdengyu <longdengyu@gmail.com>
2016-01-16 23:34:50 +0800

    完成基本插入操作

--- Request.py
+++ Request.py
@@ -13,16 +13,29 @@ class Request(object):
         # TODO:implement
     def getParents(self,child):
         parents = self.conn.execute("select parent FROM RH WHERE child = ?",(child,)).fetchall()
-        return parents
+        return [x[0] for x in parents]
     def getChildren(self, parent):
         children = self.conn.execute("select child FROM RH WHERE parent = ?",(parent,)).fetchall()
-        return children
+        return [x[0] for x in children]
+    def hasRH(self,parent,child):
+        return self.conn.execute("select parent from RH WHERE parent = ? and child = ?",\
+                                 (parent,child)).fetchone() != None
+
+    def insertUpdateRH(self,parent,child):
+        self.insertRH(parent,child)
+        self.updateRH(parent,child)
+
     def updateRH(self,parent,child):
-        pass
+        parents = self.getParents(child)
+        children = self.getChildren(parent)
+        for bigparent in parents:
+            for littlechild in children:
+                self.insertRH(bigparent,littlechild)
 
     def insertRH(self,parent,child):
-        self.conn.execute("insert into RH VALUES (?,?)",(parent, child))
-        self.conn.commit()
+        if not self.hasRH(parent,child):
+            self.conn.execute("insert into RH VALUES (?,?)",(parent, child))
+            self.conn.commit()
 
     def addUser(self,userid,name):
         self.conn.execute("insert into user VALUES (?,?)",(userid, name))
